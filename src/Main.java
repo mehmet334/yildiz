a1ec8194500fe255bd4863835b85ab65398a5589
Mehmet Ali Tezcan <mehmetalitez33@gmail.com>
2025-03-08 21:48:26 +0300

    güncelleme

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
 import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         String yildiz="";
@@ -9,14 +6,11 @@ public class Main {
         System.out.println("Sayı giriniz: ");
         int sayi= scanner.nextInt();
         for (int i = sayi; i > 0 ; i--) {
-            for (int j = 1; j <= i*2-1; j++) {
-                yildiz+="*";
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                yildiz += "*";
             }
             System.out.println(yildiz);
-            yildiz="";
+            yildiz = "";
         }
-        
-        
-        
     }
 }
